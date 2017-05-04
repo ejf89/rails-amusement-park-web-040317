ee46e583dce3077942cb7592a7367059aff3aaf9
github username <github email address>
2017-05-04 17:54:02 -0400

    changed signup route, made dropdown need to make form to submit for login page. added sign up link to welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
 
 def new
-
+@users = User.all
 end
 
 def create
 
 end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get '/signup' => 'users#new'
+  get '/users/new' => 'users#new', as: '/signup'
   post '/signup' => 'users#create'
   get '/users/:id' => 'users#show', as: 'user'
   get '/users/:id/edit' => 'users#edit'
   patch '/users/:id' => 'users#update'
+  get '/login' => 'sessions#new', as: 'login'
 
 end
